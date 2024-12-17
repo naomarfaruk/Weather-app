0a2da8e753f6ce486907711bf47798f8ff854ca8
naomarfaruk <naomarfaruk7@gmail.com>
2024-12-17 19:45:17 +0600

    update weather app

--- script.js
+++ script.js
@@ -22,15 +22,15 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML= data.wind.speed + " km/h";
     
         if (data.weather[0].main == "Clouds"){
-        weatherIcon.src = "clouds.png"
+        weatherIcon.src = "images/clouds.png"
         }else if (data.weather[0].main == "Clear"){
-            weatherIcon.src = "clear.png"
+            weatherIcon.src = "images/clear.png"
         }else if (data.weather[0].main == "Rain"){
-            weatherIcon.src = "rain.png"
+            weatherIcon.src = "images/rain.png"
         }else if (data.weather[0].main == "Drizzle"){
-            weatherIcon.src = "drizzle.png"
+            weatherIcon.src = "images/drizzle.png"
         }else if (data.weather[0].main == "Mist"){
-            weatherIcon.src = "mist.png"
+            weatherIcon.src = "images/mist.png"
         }
     
         document.querySelector(".weather").style.display = "block";
@@ -48,3 +48,5 @@ searchBox.addEventListener("keydown", function(event) {
         checkWeather(searchBox.value);
     }
   });
+
+
